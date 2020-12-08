2b708cdccf598e3bc970f73c08d99cc318fbf55d
민파파 <florakid@naver.com>
2020-12-08 14:02:48 +0900

    MyComponent Props isRequired

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 
 const App = () => {
   return (
-    <MyComponent>
-      <dov>TEST</dov>
+    <MyComponent name="advanced React" favoriteNumber={17}>
+      <div>TEST</div>
       <div>ALL TEST1</div>
       <div>ALL TEST2</div>
       <div>ALL TEST3</div>
+      <p>ALL TEST3</p>
     </MyComponent>
   );
 };
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PropTypes from "prop-types";
 
-const MyComponent = props => {
-    return (
-        <div className="react">
-            제 이름은 {props.name}입니다. <br />
-            children 값은 {props.children} 입니다.
-        </div>
-    );
+const MyComponent = ({ name, favoriteNumber, children }) => {
+  return (
+    <div className="react">
+      제 이름은 {name}입니다. <br />
+      children 값은 {children} 입니다. <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.    
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
-    name: 'basic react',
+  name: "basic react",
+  favoriteNumber: 17,
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
